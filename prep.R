e33259b8519d56339f224df3010ab22977c7fac2
Markus Kainu <markuskainu@gmail.com>
2017-04-02 19:10:09 +0300

    update

--- prep.R
+++ prep.R
@@ -11,15 +11,15 @@ wiki_names = read.csv('data/wiki_names.csv', stringsAsFactors=FALSE, na.strings=
 ## Create a new column country.name.russian.fao  for Russian language Fao statistical pocketbook
 wiki_names$country.name.russian.fao <- wiki_names$country.name.russian
 # (i) Instead of  “Республика Македония” there should be “Бывшая югославская Республика Македония”; 
-wiki_names$country.name.russian.fao[wiki_names$country.name.russian.fao == "Республика Македония"] <- "Бывшая югославская Республика Македония"
+wiki_names$country.name.russian.fao[wiki_names$country.name.russian.fao == "Республика Македония"] <- "Бывшая югославская \nРеспублика Македония"
 # (ii) instead of “Киргизия” there should be “Кыргызстан”; 
 wiki_names$country.name.russian.fao[wiki_names$country.name.russian.fao == "Киргизия"] <- "Кыргызстан"
 # (iii) Instead of “Молдавия” there should be “Республика Молдова” 
-wiki_names$country.name.russian.fao[wiki_names$country.name.russian.fao == "Молдавия"] <- "Республика Молдова"
+wiki_names$country.name.russian.fao[wiki_names$country.name.russian.fao == "Молдавия"] <- "Республика \nМолдова"
 # (iv) Instead of “Белоруссия”there should be “Беларусь”; 
 wiki_names$country.name.russian.fao[wiki_names$country.name.russian.fao == "Белоруссия"] <- "Беларусь"
 # (v) Instead of “Великобритания” there should be “Соединенное Королевство Великобритании и Северной Ирландии” (this is also indicated as short name, “Великобритания” is not an official name) ; 
-wiki_names$country.name.russian.fao[wiki_names$country.name.russian.fao == "Великобритания"] <- "Соединенное Королевство Великобритании и Северной Ирландии"
+wiki_names$country.name.russian.fao[wiki_names$country.name.russian.fao == "Великобритания"] <- "Соединенное Королевство"
 # (vi) Instead of “Республика Кипр” there should be “Кипр”; 
 wiki_names$country.name.russian.fao[wiki_names$country.name.russian.fao == "Республика Кипр"] <- "Кипр"
 # (vii) Instead of “Россия” there should be “Росиийская Федерация”; 
@@ -27,7 +27,12 @@ wiki_names$country.name.russian.fao[wiki_names$country.name.russian.fao == "Ро
 # (viii) Instead of “Туркмения” there should be “Туркменистан”; 
 wiki_names$country.name.russian.fao[wiki_names$country.name.russian.fao == "Туркмения"] <- "Туркменистан"
 # Instead of “Чехия” there should be “Чешская Республика”;
-wiki_names$country.name.russian.fao[wiki_names$country.name.russian.fao == "Чехия"] <- "Чешская Республика"
+wiki_names$country.name.russian.fao[wiki_names$country.name.russian.fao == "Чехия"] <- "Чешская \nРеспублика"
+
+wiki_names$country.name.russian.fao[wiki_names$country.name.russian.fao == "Чехия"] <- "Чешская \nРеспублика"
+
+
+
 
 countrycode_data_new <- merge(countrycode_data,wiki_names,by="country.name",all.x=TRUE)
 countrycode_data_new$country.name.english <- NULL
